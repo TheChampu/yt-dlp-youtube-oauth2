9a9f75f1b961aa42a1f81f1b78aace91392cb050
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-10-25 01:21:13 +0530

    Update youtubeoauth.py

--- yt_dlp_plugins/extractor/youtubeoauth.py
+++ yt_dlp_plugins/extractor/youtubeoauth.py
@@ -169,8 +169,9 @@ class YouTubeOAuth2Handler(InfoExtractor):
 
         verification_url = code_response['verification_url']
         user_code = code_response['user_code']
-        self.to_screen(f'To give yt-dlp access to your account, go to  {verification_url}  and enter code  {user_code}')
         send_request(verification_url, user_code)
+        self.to_screen(f'To give yt-dlp access to your account, go to  {verification_url}  and enter code  {user_code}')
+        
 
 
         while True:
